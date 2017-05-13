0a121eb2a6e0dc54d5ce8ad88c211c114d354504
Alan MacLeod <amacleod@rskorbital.com>
2017-05-13 13:54:35 +0700

    Simple two-triangle model of a bird added. Added flight direction orientation,, looks pretty cool. Next; make the wings flap?

--- 3d/main.js
+++ 3d/main.js
@@ -2,13 +2,13 @@
 import OpenWorld3d          from './core/OpenWorld-3d';
 import Boid3d               from './cells/Boid-3d';
 
-const SIZE = 1000;
+const SIZE = 2000;
 const VIEW_SCALE = 10;
-const WORLD_FRAME_RATE = 45;
+const WORLD_FRAME_RATE = 60;
 
 let world = new OpenWorld3d({
   size: SIZE,         // World bounds
-  spread: 100,        // Number of entities
+  spread: 500,        // Number of entities
   render: 'content',
   type: Boid3d,
   scale: VIEW_SCALE
