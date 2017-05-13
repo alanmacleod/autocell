40e40eb7c87612b3ef9a97bf9a9ab49872346f6d
Alan MacLeod <amacleod@rskorbital.com>
2017-05-13 14:47:28 +0700

    Flappy flap

--- 3d/main.js
+++ 3d/main.js
@@ -2,13 +2,15 @@
 import OpenWorld3d          from './core/OpenWorld-3d';
 import Boid3d               from './cells/Boid-3d';
 
+// 2000 size, 400 spread is good
+
 const SIZE = 2000;
 const VIEW_SCALE = 10;
 const WORLD_FRAME_RATE = 60;
 
 let world = new OpenWorld3d({
   size: SIZE,         // World bounds
-  spread: 500,        // Number of entities
+  spread: 400,        // Number of entities
   render: 'content',
   type: Boid3d,
   scale: VIEW_SCALE
