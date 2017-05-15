4c8ba1a4cc352b8522c71b7646fe61052e3c41e7
Alan MacLeod <amacleod@rskorbital.com>
2017-05-15 16:56:02 +0700

    `public_html` -> `pub`

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = [
         entry: './main.js',
         devtool: "inline-sourcemap",
         output: {
-          path: path.join(__dirname, "/public_html/1d/build"),
+          path: path.join(__dirname, "/pub/1d/build"),
           publicPath: "/",
           filename: "1d.js"
         },
@@ -49,7 +49,7 @@ module.exports = [
         entry: './main.js',
         devtool: "inline-sourcemap",
         output: {
-          path: path.join(__dirname, "/public_html/2d/build"),
+          path: path.join(__dirname, "/pub/2d/build"),
           publicPath: "/",
           filename: "2d.js"
         },
@@ -79,7 +79,7 @@ module.exports = [
         entry: './main.js',
         devtool: "inline-sourcemap",
         output: {
-          path: path.join(__dirname, "/public_html/3d/build"),
+          path: path.join(__dirname, "/pub/3d/build"),
           publicPath: "/",
           filename: "3d.js"
         },
